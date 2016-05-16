7d9172d66d651ed4edeb453a3faf612a9d9ce272
Manu Kumar <maraku@gmx.com>
2016-05-15 21:53:10 -0700

    task view and task completion

--- task.py
+++ task.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask
+from flask import url_for
+from flask import redirect
+from flask import render_template
+from flask import request
 from taskw import TaskWarrior
 from datetime import datetime
 import humanize
@@ -7,30 +11,29 @@ import pyjade
 # TASKWARRIOR INIT
 
 w = TaskWarrior()
-tasks = w.load_tasks()
-pending = tasks['pending']
-
-# order by date
-# if 'due' doesn't exist, then sort using a large date
-# so the task gets ordered to the bottom
-large_date = datetime(9001, 1, 1)
-pending.sort(key=lambda x: datetime.strptime(x['due'], '%Y%m%dT%H%M%SZ') if 'due' in x else large_date)
-
-# Get relative times for due dates
+pending = []
 relatimes = []
 
-got_dates = False
-for task in pending:
-    if 'due' in task:
-        task_datetime = datetime.strptime(task['due'], "%Y%m%dT%H%M%SZ")
-        relatime = humanize.naturaltime(datetime.now() - task_datetime)
-        # relatime = timesince(task_datetime)
-        got_dates = True
-    elif not got_dates:
-        relatime = "is overdue"
-    else:
-        relatime = "none"
-    relatimes.append(relatime)
+def task_load():
+    global pending
+    global relatimes
+    global w
+    tasks = w.load_tasks()
+    pending = tasks['pending']
+
+    large_date = datetime(9001, 1, 1)
+    pending.sort(key=lambda x: datetime.strptime(x['due'], '%Y%m%dT%H%M%SZ') if 'due' in x else large_date)
+
+    for task in pending:
+        if 'due' in task:
+            task_datetime = datetime.strptime(task['due'], "%Y%m%dT%H%M%SZ")
+            relatime = humanize.naturaltime(datetime.now() - task_datetime)
+        else:
+            relatime = "no time"
+        relatimes.append(relatime)
+    return
+
+task_load()
 
 # WEB SERVER
 
@@ -40,13 +43,30 @@ app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 
 @app.route('/')
 def index():
+    task_load()
     return render_template('index.jade', title = "overview",
             tasks = pending, dates = relatimes)
 
-@app.route('/tasks')
+@app.route('/task')
 def tasks():
+    task_index = int(request.args.get('index'))
+    current_task = pending[task_index]
+    relatime = relatimes[task_index]
     return render_template('task.jade', title = "task",
-            tasks = pending, date = relatimes)
+            task = current_task, due = relatime)
+
+@app.route('/sync')
+def sync():
+    w.sync()
+    return redirect(url_for('index'))
+
+@app.route('/action')
+def action():
+    action = request.args.get('action')
+    task_id = int(request.args.get('id'))
+    if action == "done":
+        w.task_done(id=task_id)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)

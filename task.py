b478246dbee6d3b936b0704648e2dbdbedb9750d
Manu Kumar <maraku@gmx.com>
2016-05-15 19:59:50 -0700

    add times for tasks

--- task.py
+++ task.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, url_for
 from taskw import TaskWarrior
 from datetime import datetime
+import humanize
 import pyjade
-
-from date_helper import prettydate;
 
 # TASKWARRIOR INIT
 
@@ -12,8 +11,10 @@ tasks = w.load_tasks()
 pending = tasks['pending']
 
 # order by date
-# if 'due' doesn't exist, then sort using the unix epoch
-pending.sort(key=lambda x: datetime.strptime(x['due'], '%Y%m%dT%H%M%SZ') if 'due' in x else datetime.fromtimestamp(0))
+# if 'due' doesn't exist, then sort using a large date
+# so the task gets ordered to the bottom
+large_date = datetime(9001, 1, 1)
+pending.sort(key=lambda x: datetime.strptime(x['due'], '%Y%m%dT%H%M%SZ') if 'due' in x else large_date)
 
 # Get relative times for due dates
 relatimes = []
@@ -22,7 +23,8 @@ got_dates = False
 for task in pending:
     if 'due' in task:
         task_datetime = datetime.strptime(task['due'], "%Y%m%dT%H%M%SZ")
-        relatime = prettydate(task_datetime)
+        relatime = humanize.naturaltime(datetime.now() - task_datetime)
+        # relatime = timesince(task_datetime)
         got_dates = True
     elif not got_dates:
         relatime = "is overdue"
@@ -30,7 +32,7 @@ for task in pending:
         relatime = "none"
     relatimes.append(relatime)
 
-# WEB
+# WEB SERVER
 
 app = Flask(__name__)
 
@@ -38,12 +40,13 @@ app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 
 @app.route('/')
 def index():
-    return render_template('index.jade', title = "tasks",
+    return render_template('index.jade', title = "overview",
             tasks = pending, dates = relatimes)
 
 @app.route('/tasks')
-def cakes():
-    return 'le tasks!'
+def tasks():
+    return render_template('task.jade', title = "task",
+            tasks = pending, date = relatimes)
 
 if __name__ == '__main__':
     app.run(debug=True)

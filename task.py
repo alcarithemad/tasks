45ba01f596ec6f6bfb62ce1321612126375484ea
Manu Kumar <maraku@gmx.com>
2016-05-15 23:28:07 -0700

    add adding tasks

--- task.py
+++ task.py
@@ -12,7 +12,6 @@ import pyjade
 
 w = TaskWarrior()
 pending = []
-relatimes = []
 
 def task_load():
     global pending
@@ -24,6 +23,7 @@ def task_load():
     large_date = datetime(9001, 1, 1)
     pending.sort(key=lambda x: datetime.strptime(x['due'], '%Y%m%dT%H%M%SZ') if 'due' in x else large_date)
 
+    relatimes = []
     for task in pending:
         if 'due' in task:
             task_datetime = datetime.strptime(task['due'], "%Y%m%dT%H%M%SZ")
@@ -55,17 +55,35 @@ def tasks():
     return render_template('task.jade', title = "task",
             task = current_task, due = relatime)
 
+@app.route('/add')
+def add():
+    return render_template('add.jade', title = "add task")
+
 @app.route('/sync')
 def sync():
     w.sync()
     return redirect(url_for('index'))
 
-@app.route('/action')
+@app.route('/action', methods=['GET', 'POST'])
 def action():
     action = request.args.get('action')
-    task_id = int(request.args.get('id'))
-    if action == "done":
-        w.task_done(id=task_id)
+    if request.method == "GET":
+        if action == "done":
+            task_id = int(request.args.get('id'))
+            w.task_done(id=task_id)
+    elif request.method == "POST":
+        action = request.form['action']
+        if action == "add":
+            task_description = request.form['description']
+            task_project = request.form['project']
+            task_date = request.form['date']
+            task_time = request.form['time']
+            fdatetime = ""
+            if task_date != "" and task_time != "":
+                pseudo_datetime = task_date + task_time
+                true_datetime = datetime.strptime(pseudo_datetime, "%Y-%m-%d%H:%M")
+                fdatetime = true_datetime.strftime("%Y%m%dT%H%M%SZ")
+            w.task_add(task_description, project=task_project, due=fdatetime)
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
